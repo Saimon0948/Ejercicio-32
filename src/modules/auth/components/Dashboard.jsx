a3f69bcb38931476198e85e8fb49c65b75ef2e87
saimon0948 <Luis.Bravo@alu.frt.utn.edu.ar>
2025-11-01 16:05:28 -0300

    commit vier

--- src/modules/auth/components/Dashboard.jsx
+++ src/modules/auth/components/Dashboard.jsx
@@ -3,11 +3,37 @@ import { NavLink } from 'react-router-dom';
 
 function Dashboard() { 
 
-
+const [open, setOpen] = React.useState(false);
     return (
-        <div>
-            <h1>Hola a Todos!</h1>
-            <ul><li><NavLink to="/login">Login</NavLink></li></ul>
+        <div className="
+        h-full
+        grid
+        grid-cols-1
+        grid-rows-[auto_1fr]
+          ">
+            <h1 className='border border-amber-400
+            flex 
+            items-center
+            justify-between
+            p-4'>
+                <span>Cabecera</span>
+                <button className='' >&#9776;</button>
+                
+            
+            
+            </h1>
+            <main className='border border-amber-600'>Contenido</main>
+            <aside className={`border border-amber-500
+            absolute
+            top-0
+            bottom-0
+            bg-gray-200
+            ${open ? 'left-0' : '-left-full'}
+            `}>
+                <ul>
+                    <li><NavLink to="/login">Login</NavLink></li>
+                </ul>
+            </aside>
         </div>
     );
 } export default Dashboard;
