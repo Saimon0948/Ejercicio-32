425963567fb21da32fd9b69b803a203eb107c440
Saimon0948 <Luis.Bravo@alu.frt.utn.edu.ar>
2025-11-01 20:06:29 -0300

    Esqueleto dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,18 @@ import Dashboard from './modules/auth/components/Dashboard';
 
 function App() {
   const routes = createBrowserRouter([
-    { path: '/', element: <LoginPage /> },
+    //{ path: '/', element: <LoginPage /> },
     { path: '/login', element: <LoginPage /> },
-    { path: '/app', element: <Dashboard /> },
+    { 
+      path: '/admin', 
+      element: <Dashboard />,
+      children: [
+        { path: 'principal', element: <div>Panel Principal</div> },
+        { path: 'productos', element: <div>Lista de Productos</div> },
+        { path: 'ordenes', element: <div>Lista de Órdenes</div> }
+      ]
+    },
+    { path: '/', element: <LoginPage /> }
   ]);
 
   return <RouterProvider router={routes} />;
--- src/modules/auth/components/Dashboard.jsx
+++ src/modules/auth/components/Dashboard.jsx
@@ -1,39 +1,68 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-function Dashboard() { 
+function Dashboard() {
+    const [open, setOpen] = React.useState(false);
+    const navClass = ({isActive}) => 
+        isActive 
+            ? 'font-bold bg-purple-300 rounded-2xl pl-10 pr-10' 
+            : '';
 
-const [open, setOpen] = React.useState(false);
     return (
-        <div className="
-        h-full
-        grid
-        grid-cols-1
-        grid-rows-[auto_1fr]
-          ">
-            <h1 className='border border-amber-400
-            flex 
-            items-center
-            justify-between
-            p-4'>
+        <div className="h-full grid grid-cols-1 grid-rows-[auto_1fr]">
+            <h1 className="border border-fuchsia-400 flex items-center justify-between p-4">
                 <span>Cabecera</span>
-                <button className='' >&#9776;</button>
-                
-            
-            
+                <button 
+                    className="bg-fuchsia-300" 
+                    onClick={() => setOpen(prev => !prev)}
+                >
+                    &#9776;
+                </button>
             </h1>
-            <main className='border border-amber-600'>Contenido</main>
-            <aside className={`border border-amber-500
-            absolute
-            top-0
-            bottom-0
-            bg-gray-200
-            ${open ? 'left-0' : '-left-full'}
+
+            <main className="border border-fuchsia-400 p-4">
+                Contenido
+            </main>
+
+            <aside className={`
+                border border-fuchsia-400
+                absolute
+                top-0
+                bottom-0
+                p-6
+                bg-gray-200
+                ${open ? 'left-0' : '-left-full'}
             `}>
+                SideBar
                 <ul>
-                    <li><NavLink to="/login">Login</NavLink></li>
+                    <li>
+                        <NavLink 
+                            to="/admin/principal"
+                            className={navClass}
+                        >
+                            Principal
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to="/admin/productos"
+                            className={navClass}
+                        >
+                            Productos
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to="/admin/ordenes"
+                            className={navClass}
+                        >
+                            Ordenes
+                        </NavLink>
+                    </li>
                 </ul>
             </aside>
         </div>
     );
-} export default Dashboard;
+}
+
+export default Dashboard;
